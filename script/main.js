637d11b0ae64f9fca621644cf90ec11494bf7ed5
Flavio-Pestana <flaviopestan@gmail.com>
2021-06-26 12:18:39 -0300

    add enemie, position, shootcheck

--- script/main.js
+++ script/main.js
@@ -1,12 +1,12 @@
 window.onload = () => {
-    document.getElementById('start-button').addEventListener("click", () => {
-        startGame();
-
-    });
+    
 
     function startGame() {
         background.draw();
-        aim.draw();
+        setTimeout ( () =>{
+            enemie1.draw()
+        },2000) 
+            
 
     }
 
@@ -21,7 +21,7 @@ window.onload = () => {
             const img = new Image();
             img.src = source;
             img.onload = () => {
-                this.img = img;
+            this.img = img;
             }
         }
 
@@ -35,12 +35,82 @@ window.onload = () => {
             );
         }
     }
-    const background = new Background('./images/gameBackground.jpg');
+    const background = new Background('./images/gameBackground.png', 0, 0, 1100, 570);
+
+    class Enemie {
+        constructor(x, y, width, height, imgsrc) {
+            this.posX = x;
+            this.posY = y;
+            this.width = width;
+            this.height = height;
+            const img = new Image();
+            img.src = imgsrc;
+            img.onload = () => {
+                this.img = img
+              
+            }
+        }
+
+        draw() {
+            ctx.drawImage(
+             this.img,
+             this.posX,
+             this.posY,
+             this.width,
+             this.height,
+             
+            )
+        };
+        
+    }
+      const enemie1 = new Enemie(250, 130, 100, 100, './images/Sprites/Enemies/enemie1.png');
+
+      
+      
+      function getCursorPosition(canvas, event) {
+        const rect = canvas.getBoundingClientRect()
+        const x = event.clientX - rect.left
+        const y = event.clientY - rect.top
+        if (x >= enemie1.posX && x <= (enemie1.width + enemie1.posX) && y >= enemie1.posY && y <= (enemie1.height + enemie1.posY) ){
+            console.log('teste');
+        } return false
+        console.log("x: " + x + " y: " + y)
+    }
+    
+        canvas.addEventListener('mousedown', function(e) {
+        getCursorPosition(canvas, e)
+    })
+
+document.getElementById('start-button').addEventListener("click", () => {
+        startGame();
+
+    });
 };
+
+// counterKills(){};
+
+
+
+//class MaidsAppears(){};
+
+// gameOver(){};
+
+/*function mudarNome()
+{
+ if(document.getElementById("button").value == "Start Game")
+ {
+  document.getElementById("button").value = "Restart";
+ }
+ else
+ {
+  document.getElementById("button").value = "Start Game";
+ }
+}*/
+
 
 // movimento mouse
 
-let mouse = {}
+/*let mouse = {}
 cnv.addEventListener('mousemove', function (event) {
     mouse.x = event.clientX - cnv.offsetLeft;
     mouse.y = event.clientY - cnv.offsetTop;
@@ -55,7 +125,7 @@ function loop() {
     requestAnimationFrame(loop, cnv);
     update();
     render();
-}
+}*/
 
 /*const mira = new Image();
   mira.onload = ()
@@ -66,6 +136,4 @@ function loop() {
 
 function update(){
 
-}
-
-*/
+}*/
